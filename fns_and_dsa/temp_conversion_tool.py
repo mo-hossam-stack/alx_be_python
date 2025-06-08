91ba4624d915e34806f9635a4e5fafa5812bce9d
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-06-08 17:11:00 +0300

    Fix: added global keyword and improved unit valudation in temo cinversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,9 +2,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
+    global FAHRENHEIT_TO_CELSIUS_FACTOR
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
+    global CELSIUS_TO_FAHRENHEIT_FACTOR
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 if __name__ == "__main__":
@@ -20,6 +22,6 @@ if __name__ == "__main__":
             result = convert_to_celsius(temperature)
             print(f"{temperature}°F is {result}°C")
         else:
-            raise ValueError("Invalid temperature unit.")
+            print("Invalid unit. Please enter C or F.")
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
