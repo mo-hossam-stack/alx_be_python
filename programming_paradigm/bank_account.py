efcb883bc788afa4f99c50e08ab03ac6c21a6da5
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-06-13 21:35:31 +0300

    Fix balance format

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,12 +1,16 @@
 class BankAccount:
-    def __init__(self,initial_balance=0):
-         self.__account_balance = initial_balance
-    def deposit(self,amount):
-        self.account_balance+=amount
-    def withdraw(self,amount):
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
         if amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         return False
+
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2f}")
+        print("Current Balance: ${:.2f}".format(self.__account_balance))
